57da33d7151bf3bd5deb288d9f7ba518670c39dd
Deon Odendaal <deon.odendaal@protonmail.com>
2019-07-07 07:44:21 +0200

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,43 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import SeasonDisplay from "./SeasonDisplay";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+class App extends React.Component {
+  // constructor function
+  // constructor(props) {
+  //   super(props);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+  //   this.state = { lat: null, errorMessage: "" };
+  // }
+
+  // Babel will compile this abbreviated line into
+  // the constructor function above
+  state = { lat: null, errorMessage: "" };
+
+  // Lifecycle methods
+  componentDidMount() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => this.setState({ lat: position.coords.latitude }),
+      err => this.setState({ errorMessage: err.message })
+    );
+  }
+
+  // componentDidUpdate() {
+  //   console.log("My component was just updated.");
+  // }
+
+  // React requires a render() method
+  render() {
+    if (this.state.errorMessage && !this.state.lat) {
+      return <div>Error: {this.state.errorMessage}</div>;
+    }
+
+    if (!this.state.errorMessage && this.state.lat) {
+      return <SeasonDisplay lat={this.state.lat} />;
+    }
+
+    return <div>Loading...</div>;
+  }
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
